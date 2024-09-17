970c80e53d7090f2e268a00dc73312b1170d4b81
Mikey <mikeybess27@gmail.com>
2024-09-17 17:02:22 -0500

    Init

---
+++ Assets/Scripts/CameraBlur.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.PostProcessing;
+
+public class CameraBlur : MonoBehaviour
+{
+	
+	[SerializeField] private float _durationInSeconds = 0.5f;
+	
+	[SerializeField] private float _startFocalLength = 50f;
+	[SerializeField] private float _targetFocalLength = 25f;
+	
+	[SerializeField] private float _startIntensity = 0.2f;
+	[SerializeField] private float _targetIntensity = 1f;
+	
+	private PostProcessLayer  v2_PostProcess;
+	
+	List<PostProcessVolume> volList = new List<PostProcessVolume>();
+	
+	private PostProcessVolume _vol;
+	private PostProcessProfile _ppp;
+	private DepthOfField _dph;
+	private Vignette _vg;
+
+
+	// Start is called on the frame when a script is enabled just before any of the Update methods is called the first time.
+	private void Start()
+	{
+		
+		v2_PostProcess = GetComponent<PostProcessLayer>();
+		
+		PostProcessManager.instance.GetActiveVolumes(v2_PostProcess, volList, true, true);
+		
+		//
+		foreach(PostProcessVolume vol in volList)
+		{
+			_vol =vol;
+			PostProcessProfile ppp = vol.profile;
+			if (ppp) 
+			{
+				_ppp = ppp;
+				DepthOfField dph;
+				if (ppp.TryGetSettings<DepthOfField>(out dph))
+				{
+					_dph = dph;
+				}
+				
+				Vignette vg;
+				if (ppp.TryGetSettings<Vignette>(out vg))
+				{
+					_vg = vg;
+				}
+			}
+		}
+		
+		_dph.focalLength.value = 0;
+	}
+	
+	private void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Z))
+		{
+			StopAllCoroutines();
+			StartCoroutine(Focus(_durationInSeconds));
+		}
+		
+		if(Input.GetKeyUp(KeyCode.Z))
+		{
+			StopAllCoroutines();
+			StartCoroutine(UnFocus(_durationInSeconds));
+		}
+		
+		if(Input.GetKeyDown(KeyCode.X))
+		{
+			_dph.focalLength.value = 0;
+		}
+		
+		if(Input.GetKeyDown(KeyCode.C))
+		{
+			_dph.focalLength.value = 50;
+		}
+	}
+
+	private IEnumerator Focus(float blurTime)
+	{
+		float timeCount = 0;
+		
+		float currentFocalLength = _dph.focalLength.value;
+		float currentIntensity = _vg.intensity.value;
+		
+		while(timeCount < blurTime)
+		{
+			timeCount += Time.deltaTime;
+			yield return null;
+			_dph.focalLength.value = (Mathf.Lerp(currentFocalLength, _targetFocalLength, timeCount / blurTime));
+			_vg.intensity.value = (Mathf.Lerp(currentIntensity, _targetIntensity, timeCount / blurTime));
+
+		}
+	}
+	
+	private IEnumerator UnFocus(float blurTime)
+	{
+		float timeCount = 0;
+		
+		float currentFocalLength = _dph.focalLength.value;
+		float currentIntensity = _vg.intensity.value;
+		
+		while(timeCount < blurTime)
+		{
+			timeCount += Time.deltaTime;
+			yield return null;
+			_dph.focalLength.value = (Mathf.Lerp(currentFocalLength, _startFocalLength, timeCount / blurTime));
+			_vg.intensity.value = (Mathf.Lerp(currentIntensity, _startIntensity, timeCount / blurTime));
+		}
+	}
+
+}
