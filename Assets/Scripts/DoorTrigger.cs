4e58fb723c2e585acaec4e300add698ce6a49997
Mikey Bess <mikeybess27@gmail.com>
2024-10-17 09:39:43 -0500

    Better Door Trigger, Fixed UI Distance

--- Assets/Scripts/DoorTrigger.cs
+++ Assets/Scripts/DoorTrigger.cs
@@ -3,55 +3,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorTrigger : MonoBehaviour
+namespace VHS
 {
-    [SerializeField] private GameObject _doorLeft;
-    [SerializeField] private GameObject _doorRight;
-    
-    private Vector3 _doorLeftOpenPos;
-    private Vector3 _doorRightOpenPos;
-    
-    private BoxCollider _bc;
-    private bool _isPlayerInside;
-
-    private void Awake()
+    public class DoorTrigger : InteractableBase
     {
-        _bc = GetComponent<BoxCollider>();
-        _doorLeftOpenPos = new Vector3(25.775f, -1, 5.654f);
-        _doorRightOpenPos = new Vector3(30f, -1, 1.44f);
-    }
+        [SerializeField] private GameObject _doorLeft;
+        [SerializeField] private GameObject _doorRight;
 
-    private void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.E) && _isPlayerInside)
+        private Vector3 _doorLeftOpenPos;
+        private Vector3 _doorRightOpenPos;
+
+        private void Awake()
         {
+            _doorLeftOpenPos = new Vector3(25.775f, -1, 5.654f);
+            _doorRightOpenPos = new Vector3(30f, -1, 1.44f);
+        }
+
+        public override void  OnInteract()
+        {
+            base.OnInteract();
+
             StartCoroutine(DoorOpen());
         }
-    }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.CompareTag("Player"))
+        private IEnumerator DoorOpen()
         {
-            _isPlayerInside = true;
-        }
-    }
-    
-    private void OnTriggerExit(Collider other)
-    {
-        if(other.CompareTag("Player"))
-        {
-            _isPlayerInside = false;
-        }
-    }
+            while (Vector3.Distance(_doorLeft.transform.localPosition, _doorLeftOpenPos) > 0.1f)
+            {
+                _doorLeft.transform.localPosition = Vector3.Lerp(_doorLeft.transform.localPosition, _doorLeftOpenPos, Time.deltaTime * 2);
+                _doorRight.transform.localPosition = Vector3.Lerp(_doorRight.transform.localPosition, _doorRightOpenPos, Time.deltaTime * 2);
+                yield return null;
+            }
 
-    private IEnumerator DoorOpen()
-    {
-        while(Vector3.Distance(_doorLeft.transform.localPosition, _doorLeftOpenPos) > 0.1f)
-        {
-            _doorLeft.transform.localPosition = Vector3.Lerp(_doorLeft.transform.localPosition, _doorLeftOpenPos, Time.deltaTime * 2);
-            _doorRight.transform.localPosition = Vector3.Lerp(_doorRight.transform.localPosition, _doorRightOpenPos, Time.deltaTime * 2);
-            yield return null;
+            Destroy(this);
         }
     }
 }
